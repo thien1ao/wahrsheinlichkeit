4b2045a8484af7e1583a9a96e3020883e212de37
thien1ao <mischsafonov4@gmail.com>
2025-03-17 17:36:41 +0100

    second

--- gender.py
+++ gender.py
@@ -1,69 +1,45 @@
-import random
+from scipy import stats
 import numpy as np
+import matplotlib as plt
+import random
 
-N = 200
+N = 1000
 U_M = 170
 O_M = 9
 U_W = 160
 O_W = 8
-ARR_SIZE = 100
-RAND_FIRST_N = 10
-MIN_KÖRPERGRÖSSE = 150
-MAX_KÖRPERGRÖSSE = 200
-
-"""
-:param const int N  Anzahl der Studierende in Hörsall
-:param const int U_M  Erwartungswert  Männer
-:param const int O_M  Standartverteilung Männer
-:param const int U_W Erwartungswert Frauen
-:param const int O_W Standartverteilung Frauen
-"""
-
-norm_arr = []
+SIMULATIONEN = 10000
 
 
-for _ in range(ARR_SIZE // 2):
-    norm_arr.append(('M', np.random.normal(U_M, O_M)))
+männer = np.random.normal(U_M, O_M, N)
+frauen = np.random.normal(U_W, O_W, N)
+
+mann_list = []
+frau_list = []
 
 
-for _ in range(ARR_SIZE // 2):
-    norm_arr.append(('W', np.random.normal(U_W, O_W)))
+def __iter__():
+    frau_count = 0
+    for i in range(SIMULATIONEN):
+        mann = random.choice(männer)
+        frau = random.choice(frauen)
 
-#rand_10_arr = arr[:RAND_FIRST_N]
+        if (frau > mann):
+            frau_count += 1
 
-#print(rand_10_arr)
+    print(f"Wahrscheinlichkeit: {frau_count / SIMULATIONEN:.2f}")
 
-def compare():
-    mann =  random.choice([i for i in norm_arr if i[0] == 'M'])
-    frau = random.choice([i for i in norm_arr if i[0] == 'W'])
 
-    print(f"Mann: {mann[1]:.2f}, Frau: {frau[1]:.2f}")
+__iter__()
 
-def calculate_p(norm_arr):
-    count = 0
 
-    for i in range(len(norm_arr) - 1):
-        mann = norm_arr[i]
-        frau = norm_arr[i + 1]
 
-        if frau > mann:
-            count += 1
 
-    p = count / N
 
-    #for _ in range(N):
-    #    mann = np.random.normal(U_M, O_M)
-    #    frau = np.random.normal(U_W, O_W)
 
-    #    if frau > mann:
-    #        count += 1
 
-    # p = count / N
 
-    print(f"p: {p:.2f}")
 
-compare()
-calculate_p(norm_arr)
 
 
 
